6c8bd1fe52e8220ce96bfe6941ed875fba793cfc
Donovan Griego <dxgriego@gmail.com>
2021-04-18 19:18:33 -0600

    Made changes to main.c

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
         printf("usage: main -s size -f function -i iterations\n");
         exit(EXIT_FAILURE);
     }
-    while((c = getopt(argc, argv, ":s:f:i")) != -1) {
+    while((c = getopt(argc, argv, "s:f:i:")) != -1) {
         printf("Checkpoint 0\n");
         switch(c) {
             case 's':
